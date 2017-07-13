bdef785b40a7c20e81f07e2c908c2115cca55fb3
Kaspars Andzans <kaspars_andzans@inbox.lv>
2017-07-13 09:53:01 +0000

    commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+    </head>
+    
+    <body>
+        <h1>Welcome to Laravel</h1>
+        <p>This is the laravel application from "Laravel From Scratch" Youtube series</p>
+    </body>
+
+</html>
---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <title>Services</title>
+    </head>
+    
+    <body>
+        <h1>Services</h1>
+        <p>This is the services page</p>
+    </body>
+
+</html>
